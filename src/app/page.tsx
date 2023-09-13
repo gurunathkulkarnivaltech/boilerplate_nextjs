f7c0f1b51901cdb74f1a2f3bcd276ce6e07046dd
Gurunath Kulkarani <gurunathkulkarani@IN-FR7X6DG7MR.local>
2023-09-13 17:02:32 +0530

    intial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+
+interface Props {}
+
+function App({}: Props) {
+  return <div>page</div>;
+}
+
+export default App;
---
+++ src/utils/api/apiConstants.ts
@@ -0,0 +1,5 @@
+export const BASE_URL = process.env.BASE_URL || ``;
+
+// example for URL
+
+export const WHATTYPEOF_URL = `${BASE_URL}/end_point`;
